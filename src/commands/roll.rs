0f2306adf085d884aba1e12cf8ab612a8f945ee2
Derrek <dschuurm@ucsd.edu>
2020-06-22 11:29:08 -0700

    added + or - feature to rolls

--- src/commands/roll.rs
+++ src/commands/roll.rs
@@ -22,34 +22,32 @@ fn roll(ctx: &mut Context, msg: &Message) -> CommandResult {
 	let mut print_string: String = String::new();
 	let mut a_flag = false;
 
-
-
     let tuple_return = parse::parse_roll_message(message_string);
-	
-	if tuple_return.3 != ""  {
-        print_string = tuple_return.3;
+
+    let num_roll = tuple_return.0;
+    let dice_type = tuple_return.1;
+    let add_on = tuple_return.2;
+    let args = tuple_return.3;
+    let err_string = tuple_return.4;
+
+    if err_string != ""  {
+        print_string = err_string;
     }  else  {
-        for character in tuple_return.2  {
+        for character in args  {
             if character == 'a'  {
                 a_flag = true;
             }
         }
 
         if a_flag == true  {
-            println!("avg");
-            print_string = roller::avg_roller(tuple_return.0, tuple_return.1);
+            print_string = roller::avg_roller(num_roll, dice_type, add_on);
         }  else  {
-          print_string = roller::print_all_rolls(tuple_return.0, tuple_return.1);
+          print_string = roller::print_all_rolls(num_roll, dice_type, add_on);
         }
         
     }
 	
-	
     msg.reply(ctx, print_string);
-	
-	
-	
-	
 	
     Ok(())
 }
